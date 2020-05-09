ac6d424da3adb77a2cd05c5f85c03f1d1f3637d1
huusseins <hussein.s.muhammad@gmail.com>
2020-05-10 02:07:40 +0700

    Tambahin setting responsive, buat data JSON, gabungin JSON dgn HTML

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Navbar
-.btnSearch {
-    color: white;
-    border: white;
-}
+$.getJSON('destinasi.json', function (data) {
+    let destinasi = data.destinasi;
+    $.each(destinasi, function (i, data) {
+        $('#destination-list').append('<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt="Cappadocia"><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">See more </button><a href="#" class="btn btn-outline-danger btn-add-wish">Add to wishlist</a></div></div>')
+    });
+});
