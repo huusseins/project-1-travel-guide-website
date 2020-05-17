18820ed990a2fd97eb62d82cc1934a6bfca54031
huusseins <hussein.s.muhammad@gmail.com>
2020-05-17 14:22:01 +0700

    Menghilangkan bug, menambah fitur add wishlist dan remove wishlist, memperbaiki fitur search

--- script.js
+++ script.js
@@ -3,90 +3,112 @@ function showAllCard() {
     $.getJSON('destinasi.json', function (data) {
         let destinasi = data.destinasi;
         $.each(destinasi, function (i, data) {
-            $('#destination-list').append('<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary" id="btn-modal" data-toggle="modal" data-target="#exampleModalCenter">See more </button><a href="#" class="btn btn-outline-danger">Add to wishlist</a></div></div>')
-        });
-    });
+            $('#destination-list').append('<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary totalExpenses">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary button-modal-detail" data-toggle="modal" data-target="#destinationDetailModal" data-destinationid="' + data.attraction + '">See more </button><button type="button" class="btn btn-outline-danger btn-wishlist">Add to wishlist</button></div></div>')
+        })
+    })
 }
 
-showAllCard();
+showAllCard()
 
-// Filter dropdown
-$('.dropdown-item').on('click', function () {
-    let negara = $(this).html();
+$(document).ready(function () {
 
-    if (negara == 'All location') {
+    // Search bar
+    const handleSearchBox = () => {
+        let searchTextValue = document.querySelector('#searchText').value
+        let cardDeck = document.querySelector('#destination-list').children
 
-        $("#destination-list").html("");
-        showAllCard();
-        $('.dropdown-toggle').html('All location');
-        return;
-    };
-    $.getJSON('destinasi.json', function (data) {
-        let destinasi = data.destinasi;
-        let content = '';
+        for (let i = 0; i < cardDeck.length; i++) {
 
-        $.each(destinasi, function (i, data) {
-            if (data.country == negara) {
-                content += '<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary" id="btn-modal" data-toggle="modal" data-target="#exampleModalCenter">See more </button><a href="#" class="btn btn-outline-danger">Add to wishlist</a></div></div></div>';
-            }
-        });
-        $('#destination-list').html(content);
-        $('.dropdown-toggle').html(negara);
-    });
-});
+            const title = cardDeck[i].children[0].children[1].children[0];
+            let valueTitle = title.innerHTML;
+            const subtitle = cardDeck[i].children[0].children[1].children[1];
+            let valueSubtitle = subtitle.innerHTML;
 
-
-// $('#btn-modal').on('click', function () {
-//     let contentCard = $(this).html();
-//     console.log(contentCard);
-// $.getJSON('destinasi.json', function (data) {
-//     let destinasi = data.destinasi;
-
-//     $.each(destinasi, function (i, data) {
-//         if (data.attraction == contentCard) {
-//             $('.modal-content').append('<div class="modal-header"><h5 class="modal-title" id="exampleModalCenterTitle"> ' + data.attraction + ' </h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img class="img-fluid" src="img/ ' + data.image + ' " alt=" ' + data.attraction + ' "><br><br><h5 class="card-subtitle"> ' + data.location + ', ' + data.country + ' </h5><br><p class="card-text"> ' + data.description + ' </p></div><div class="modal-footer"><div class="ket-expenses-modal"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$ ' + data.price + ' </span></div><button type="button" class="btn btn-outline-danger btn-add-wish">Add to wishlist</button></div>')
-//         }
-//     });
-// });
-// });
-
-// Search bar
-$("#searchButton").on("click", function () {
-    $("#destination-list").html("");
-    var textValue = $("#searchText").val();
-    var res = textValue.toLowerCase();
-
-    var state = false;
-    $.getJSON('destinasi.json', function (data) {
-        let destinasi = data.destinasi;
-        $.each(destinasi, function (i, data) {
-            var country = data.country.toLowerCase();
-            var attraction = data.attraction.toLowerCase();
-            var location = data.location.toLowerCase();
-            var summary = data.summary.toLowerCase();
-            var price = data.price;
-
-            if (res == null) {
-                state = true;
-                $('#destination-list').append('<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary" id="btn-modal" data-toggle="modal" data-target="#exampleModalCenter">See more </button><a href="#" class="btn btn-outline-danger">Add to wishlist</a></div></div>')
+            if (valueTitle.toLowerCase().indexOf(searchTextValue.toLowerCase()) > -1 || valueSubtitle.toLowerCase().indexOf(searchTextValue.toLowerCase()) > -1) {
+                title.parentElement.parentElement.parentElement.style.display = ''
             } else {
-                //filter
-                if (country.includes(res) || attraction.includes(res) || location.includes(res) || summary.includes(res) || price == res) {
-                    state = true;
-                    $('#destination-list').append('<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary" id="btn-modal" data-toggle="modal" data-target="#exampleModalCenter">See more </button><a href="#" class="btn btn-outline-danger">Add to wishlist</a></div></div>')
-                }
+                title.parentElement.parentElement.parentElement.style.display = 'none'
             }
-        });
-        if (state == false) {
-            $('#destination-list').append('<h5 class="text-center"> Oops, the destination you were looking for doesn\'t exist.</h5>')
         }
-    });
-});
-
-var input = document.getElementById("searchText");
-input.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("searchButton").click();
     }
-});
+
+    let searchText = document.querySelector('#searchText')
+    searchText.addEventListener('keyup', handleSearchBox)
+
+    // Filter dropdown
+    $('.dropdown-item').on('click', function () {
+        let negara = $(this).html();
+
+        if (negara == 'All location') {
+
+            $('#destination-list').html('');
+            showAllCard();
+            $('.dropdown-toggle').html('All location');
+            return;
+        };
+        $.getJSON('destinasi.json', function (data) {
+            let destinasi = data.destinasi;
+            let content = '';
+
+            $.each(destinasi, function (i, data) {
+                if (data.country == negara) {
+                    content += '<div class="col-sm"><div class="card"><img class="img-fluid" src="img/' + data.image + '" alt=""><div class="card-body"><h5 class="card-title"> ' + data.attraction + '</h5><p class="card-subtitle text-muted"> ' + data.location + ', ' + data.country + ' </p><br><p class="card-text"> ' + data.summary + ' </p></div><div class="card-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary totalExpenses">US$  ' + data.price + ' </span><br><br><button type="button" class="btn btn-primary button-modal-detail" data-toggle="modal" data-target="#destinationDetailModal" data-destinationid="' + data.attraction + '">See more </button><button type="button" class="btn btn-outline-danger btn-wishlist">Add to wishlist</button></div></div></div>';
+                }
+            })
+            $('#destination-list').html(content)
+            $('.dropdown-toggle').html(negara)
+        })
+    })
+
+    // Modal
+    $('#destination-list').on('click', '.button-modal-detail', function () {
+        let modalId = ($(this).data('destinationid'))
+
+        $.getJSON('destinasi.json', function (data) {
+            let destinasi = data.destinasi;
+            $.each(destinasi, function (i, data) {
+                if (data.attraction == modalId) {
+                    $('.modal-dialog').html('<div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="destinationDetailModalTitle">' + data.attraction + '</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><img class="img-fluid" src="img/' + data.image + '" alt="' + data.attraction + '"><br><br><h5 class="card-subtitle">' + data.location + ', ' + data.country + '</h5><br><p class="card-text">' + data.description + '</p></div><div class="modal-footer"><span class="h6">Total expenses: </span><span class="h6 text-primary totalExpenses">US$ ' + data.price + '</span><button type="button" class="btn btn-outline-danger btn-wishlist">Add to wishlist</button></div></div></div>')
+                }
+            })
+        })
+    })
+
+    // Update total price
+    function updatePrice() {
+        let updatePrice = document.querySelector('.totalPrice')
+        let hiddenDiv = document.querySelector('.hiddenDiv').children
+        let total = 0;
+
+        for (let i = 0; i < hiddenDiv.length; i++) {
+            const cost = hiddenDiv[i].innerHTML
+            total = total + parseInt(cost)
+        }
+        if (updatePrice.value != '0') {
+            updatePrice.value = 'Total price: US$ ' + total;
+        }
+    }
+
+    // Add wishlist
+    $('#destination-list, .modal-dialog').on('click', '.btn-wishlist', function () {
+        let totalExpenses = parseInt($(this).siblings('.totalExpenses').html().slice(4))
+        $(this).removeClass('btn-outline-danger btn-wishlist').addClass('btn-danger wishlist-active').html('Remove from wishlist')
+        $('.hiddenDiv').append('<div></div>')
+        $('.hiddenDiv').children().last().append(totalExpenses)
+        updatePrice()
+    })
+
+    // Remove wishlist
+    $('#destination-list, .modal-dialog').on('click', '.wishlist-active', function () {
+        let totalExpenses = parseInt($(this).siblings('.totalExpenses').html().slice(4))
+        $(this).removeClass('btn-danger wishlist-active').addClass('btn-outline-danger btn-wishlist').html('Add to wishlist')
+        let hiddenDiv = document.querySelector('.hiddenDiv').children
+        for (let i = 0; i < hiddenDiv.length; i++) {
+            if (hiddenDiv[i].innerHTML == totalExpenses) {
+                hiddenDiv[i].remove()
+                break;
+            }
+        }
+        updatePrice();
+    })
+})
